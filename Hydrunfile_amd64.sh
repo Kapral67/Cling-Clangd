10850deb97f5d8c4368daad0ca1244971f952edd
Maxwell Kapral <kapralmaxwell@gmail.com>
2021-08-18 02:54:43 -0700

    cleanup

--- Hydrunfile_amd64.sh
+++ Hydrunfile_amd64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y git cmake build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig doxygen graphviz xz-utils ninja-build software-properties-common lsb-release gcc g++ sed sudo
+apt install -y git build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig graphviz xz-utils ninja-build gcc g++ sed sudo
 
 yes | pip3 install --upgrade cmake
 
@@ -13,18 +13,18 @@ git clone --branch llvmorg-12.0.1 https://github.com/llvm/llvm-project.git
 # Edit for Clangd to work with Jupyter .ipynb
 sed -i "265i\\\t   .Case(\"ipynb\", TY_CXX)" /tmp/llvm-project/clang/lib/Driver/Types.cpp
 
-mkdir -p /out/llvm
 mkdir -p /out/build
 cd /out/build
-cmake /tmp/llvm-project/llvm -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_ENABLE_ASSERTS=On -DCMAKE_INSTALL_PREFIX=/out/llvm
-ninja check-clangd
+cmake /tmp/llvm-project/llvm -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
+ninja clangd
 rm -rf /tmp/llvm-project
 
 mkdir -p /out/data/build/bin
 mkdir -p /out/data/lib/clang
 mv /out/build/bin/clangd /out/data/build/bin
 mv /out/build/lib/clang/* /out/data/lib/clang
-tar -zcf /out/data/jupyter-clangd.$(uname -m).tar.gz /out/data/*
+cd /out/data
+tar -zcf ./jupyter-clangd.$(uname -m).tar.gz *
 rm -rf /out/build
 
 mkdir -p /data/out
--- Hydrunfile_arm64.sh
+++ Hydrunfile_arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y git cmake build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig doxygen graphviz xz-utils ninja-build software-properties-common lsb-release gcc g++ sed sudo
+apt install -y git build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig graphviz xz-utils ninja-build gcc g++ sed sudo
 
 yes | pip3 install --upgrade cmake
 
@@ -13,7 +13,6 @@ git clone --branch llvmorg-12.0.1 https://github.com/llvm/llvm-project.git
 # Edit for Clangd to work with Jupyter .ipynb
 sed -i "265i\\\t   .Case(\"ipynb\", TY_CXX)" /tmp/llvm-project/clang/lib/Driver/Types.cpp
 
-mkdir -p /out/llvm
 mkdir -p /out/build
 cd /out/build
 cmake /tmp/llvm-project/llvm -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
@@ -24,7 +23,8 @@ mkdir -p /out/data/build/bin
 mkdir -p /out/data/lib/clang
 mv /out/build/bin/clangd /out/data/build/bin
 mv /out/build/lib/clang/* /out/data/lib/clang
-tar -zcf /out/data/jupyter-clangd.$(uname -m).tar.gz /out/data/*
+cd /out/data
+tar -zcf ./jupyter-clangd.$(uname -m).tar.gz *
 rm -rf /out/build
 
 mkdir -p /data/out
