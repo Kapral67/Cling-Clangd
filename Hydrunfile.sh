30e9af1b8a5a35383b6731aec9c81672cc44ec5f
Maxwell Kapral <mkapral@mail.csuchico.edu>
2021-08-22 04:43:43 -0700

    - graphviz - swig

--- Hydrunfile.sh
+++ Hydrunfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig graphviz xz-utils ninja-build gcc g++ sed curl wget
+apt update && DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev xz-utils ninja-build gcc g++ sed curl wget
 
 yes | pip3 install --upgrade cmake
 
@@ -9,7 +9,7 @@ export CXX=/usr/bin/g++
 cd /tmp
 export url=$(curl -s https://api.github.com/repos/llvm/llvm-project/releases/latest | grep "tarball_url" | cut -d '"' -f 4,4)
 wget -O latest.tar.gz $url
-tar -zvxf latest.tar.gz
+tar -zxf latest.tar.gz
 export llvm=$(find . -type d -name *llvm* -print)
 
 export version=$(curl -s https://api.github.com/repos/llvm/llvm-project/releases/latest | grep "tag_name" | cut -d '"' -f 4,4 | cut -d '-' -f 2)
@@ -29,7 +29,7 @@ mv /tmp/build/bin/clangd /tmp/data/build/bin
 mv /tmp/build/lib/clang/* /tmp/data/lib/clang
 cd /tmp/data
 curl -so LICENSE.txt https://raw.githubusercontent.com/Kapral67/Cling-Clangd/19fad1da46dbb76baa9a46ceb4971e92c2d9ee46/LICENSE
-tar -zvcf ./xeus-clangd.$version.$(uname -m).tar.gz *
+tar -zcf ./xeus-clangd.$version.$(uname -m).tar.gz *
 rm -rf /tmp/build
 
 mkdir -p /data/out
