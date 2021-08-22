03282ddca7d254e3b53ac0c702a2798cb61e9a66
Maxwell Kapral <mkapral@mail.csuchico.edu>
2021-08-22 01:55:57 -0700

    Build LICENSE.txt & Cleanup

--- Hydrunfile_amd64.sh
+++ Hydrunfile_amd64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends git build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig graphviz xz-utils ninja-build gcc g++ sed sudo
+apt update && DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends git build-essential subversion python3-dev python3-pip libncurses5-dev libxml2-dev libedit-dev swig graphviz xz-utils ninja-build gcc g++ sed sudo wget
 
 yes | pip3 install --upgrade cmake
 
@@ -12,19 +12,20 @@ git clone --branch llvmorg-12.0.1 https://github.com/llvm/llvm-project.git
 # Edit for Clangd to work with Jupyter .ipynb
 sed -i "265i\\\t   .Case(\"ipynb\", TY_CXX)" /tmp/llvm-project/clang/lib/Driver/Types.cpp
 
-mkdir -p /out/build
-cd /out/build
+mkdir -p /tmp/build
+cd /tmp/build
 cmake /tmp/llvm-project/llvm -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"
 ninja clangd
 rm -rf /tmp/llvm-project
 
-mkdir -p /out/data/build/bin
-mkdir -p /out/data/lib/clang
-mv /out/build/bin/clangd /out/data/build/bin
-mv /out/build/lib/clang/* /out/data/lib/clang
-cd /out/data
+mkdir -p /tmp/tmp/build/bin
+mkdir -p /tmp/tmp/lib/clang
+mv /tmp/build/bin/clangd /tmp/tmp/build/bin
+mv /tmp/build/lib/clang/* /tmp/tmp/lib/clang
+cd /tmp/tmp
+wget -O LICENSE.txt https://raw.githubusercontent.com/Kapral67/Cling-Clangd/19fad1da46dbb76baa9a46ceb4971e92c2d9ee46/LICENSE
 tar -zcf ./jupyter-clangd.$(uname -m).tar.gz *
-rm -rf /out/build
+rm -rf /tmp/build
 
 mkdir -p /data/out
-mv /out/data/jupyter-clangd.$(uname -m).tar.gz /data/out
+mv /tmp/tmp/jupyter-clangd.$(uname -m).tar.gz /data/out
